4fcd7de64e2b20b76355c403efebbab1194dfbbe
mynul5837 <islam15-5837@diu.edu.bd>
2025-02-23 09:26:10 +0600

    a-card

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyWork());
+  runApp(MyWork());
 }
 
 class MyWork extends StatelessWidget {
-  const MyWork({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Work'),
+        ),
+        body: Center(
+          child: ColorfulCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorfulCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 5,
+      child: Container(
+        width: 300,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.blue, Colors.green],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.star,
+                color: Colors.white,
+                size: 40,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'My Work',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Mynul islam made this',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
