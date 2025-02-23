726583a285298db411b7b2a17c8b59bdb52f2fe8
mynul5837 <islam15-5837@diu.edu.bd>
2025-02-23 09:11:45 +0600

    today

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWork());
+}
+
+class MyWork extends StatelessWidget {
+  const MyWork({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
