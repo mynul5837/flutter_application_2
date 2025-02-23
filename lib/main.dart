15394db5bb29b3613605916339c9e1730b696b99
mynul5837 <islam15-5837@diu.edu.bd>
2025-02-23 09:47:53 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyWork());
+  runApp(MyPro());
 }
 
-class MyWork extends StatelessWidget {
+class MyPro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Advanced UI Lab 06'),
-        ),
-        body: Center(
-          child: ColorfulCard(),
+          title: Text('Advanced Home Assistance'),
         ),
       ),
     );
   }
 }
 
-class ColorfulCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      elevation: 5,
-      child: Container(
-        width: 300,
-        height: 150,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          gradient: LinearGradient(
-            colors: [Colors.purple, Colors.blue, Colors.green],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Icon(
-                Icons.star,
-                color: Colors.white,
-                size: 40,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Advanced UI',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                'LAB 06',
-                style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
